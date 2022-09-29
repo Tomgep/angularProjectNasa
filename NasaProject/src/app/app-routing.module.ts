c53118e6d5dc1b9d93783644956b0b732707c465
lolo <63410056+loicdevco@users.noreply.github.com>
2022-09-29 16:29:04 +0200

    Mise en place des components

--- NasaProject/src/app/app-routing.module.ts
+++ NasaProject/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphComponent } from './graph/graph.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'graph', component: GraphComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
